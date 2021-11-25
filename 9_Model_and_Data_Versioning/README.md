60c12bcaeff625b82be63b687778eddd1cba9b1a
theyorubayesian <akin.o.oladipo@gmail.com>
2021-11-25 01:49:45 +0100

    created basic structure

--- 9_Model_and_Data_Versioning/README.md
+++ 9_Model_and_Data_Versioning/README.md
@@ -4,7 +4,12 @@
 * If you are using Windows, it is recommended that you instal the Ubuntu 20.04 LTS as Linux Distribution. You can find instructions here: [Install WSL](https://docs.microsoft.com/en-us/windows/wsl/install)
 * Install Anaconda or Miniconda on your laptop. To check if you have it installed, run
 ```sh
-conda 
+conda env create --name insurtech --file conda.yml
 ```
+To understand how the `conda env create`command works, run `conda env create --help`.
 * Create a [Weights & Biases](https://wandb.ai/) account
 * 
+
+
+# Resources
+1. [How we build MLflow projects and rapidly iterate over them](https://medium.com/hellogetsafe/how-we-build-mlflow-projects-and-rapidly-iterate-over-them-90009de824cf)
---
+++ 9_Model_and_Data_Versioning/main.py
@@ -0,0 +1,51 @@
+import json
+import os
+import tempfile
+
+import mlflow
+from mlflow.entities import param
+import yaml
+
+STEPS = [
+    "download",
+    "cleaning",
+    "data_validation",
+    "data_split",
+    "train_model"
+]
+
+ROOT_PATH = os.getcwd()
+
+
+def main():
+    # TODO: Use `python-dotenv` to put these variables into the environment
+    # then remove this bit of code 
+    os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
+    os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
+
+    steps = config["main"]["steps"]
+    active_steps = steps.split(",") if steps != "all" else STEPS
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        if "download" in active_steps:
+            _ = mlflow.run(
+                os.path.join(ROOT_PATH, config["main"]["src"], "download"),
+                "main",
+                parameters={
+                    "data": config["etl"]["data"],
+                    "artifact_name": "input_data.csv",
+                    "artifact_type": "raw_data",
+                    "artifact_description": "Downloaded input data"
+                }
+            )
+
+
+
+if __name__ == "__main__":
+    with open("config.yml", "r") as f:
+        try:
+            config = yaml.safe_load(f)
+        except yaml.YAMLError as err:
+            print(err)
+            raise err
+
